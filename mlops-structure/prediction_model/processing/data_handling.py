030dcd5091145e619e08d5df2b6112855588245b
Abhay-C5 <151907591+Abhay-C5@users.noreply.github.com>
2024-07-04 14:57:01 +0000

    updated data handling

--- mlops-structure/prediction_model/processing/data_handling.py
+++ mlops-structure/prediction_model/processing/data_handling.py
@@ -8,13 +8,13 @@ def load_dataset(file_name):
     _data = pd.read_csv(filepath)
     return _data
 
-#function to save pickle file from model
-
+#function to save pickle file from model i.e. serialization of the model
 def save_pipeline(pipeline_to_save):
     save_path = os.path.join(config.SAVE_MODEL_PATH, config.MODEL_NAME)
     joblib.dump(pipeline_to_save, save_path)
     print(f"Your Model has been saved successfully under the name {config.MODEL_NAME}")
 
+#function to load the pickle i.e. desearialization of the model
 def load_pipeline(pipeline_to_load):
     load_path = os.path.join(config.SAVE_MODEL_PATH, config.MODEL_NAME)
     model_loaded = joblib.load(load_path)
